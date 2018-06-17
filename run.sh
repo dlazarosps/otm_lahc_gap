844e4c7c64622925daa7885ddd4115cdfcf10b59
dlazarosps <dlspsilva@inf.ufrgs.br>
2018-06-16 23:38:02 -0300

    [MOVE] output results subpath
    (update) run script

--- run.sh
+++ run.sh
@@ -2,22 +2,24 @@
 echo "gapa-1"
 instances="gapa-1.txt"
 
-limit=500000
-alpha=90
+limit=1000000
 # output="$outDir-limit-$limit-list-$list-alpha-$alpha-in-$instances"
 
-output="output-limit-500000-list-300-alpha-10-in-$instances"
-./bin/main 500000 300 10 < ./instances/$instances > ./output/$output
-echo $output
+# output="output-limit-1000000-list-300-alpha-10-in-$instances"
+# ./bin/main 1000000 300 10 < ./instances/$instances > ./output/$output
+# echo $output
 
-output="output-limit-500000-list-300-alpha-30-in-$instances"
-./bin/main 500000 300 30 < ./instances/$instances > ./output/$output
-echo $output
+# output="output-limit-1000000-list-300-alpha-30-in-$instances"
+# ./bin/main 1000000 300 30 < ./instances/$instances > ./output/$output
+# echo $output
 
-output="output-limit-500000-list-1000-alpha-70-in-$instances"
-./bin/main 500000 1000 70 < ./instances/$instances > ./output/$output
-echo $output
+# output="output-limit-1000000-list-1000-alpha-70-in-$instances"
+# ./bin/main 1000000 1000 70 < ./instances/$instances > ./output/$output
+# echo $output
 
+# ----------------------------------------------------------------------- #
+
+alpha=10
 list=50
 output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
 ./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
@@ -48,3 +50,134 @@ output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
 ./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
 echo $output
 
+# ----------------------------------------------------------------------- #
+
+alpha=30
+list=50
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=100
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=200
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=300
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=500
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=1000
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+# ----------------------------------------------------------------------- #
+
+alpha=50
+list=50
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=100
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=200
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=300
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=500
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=1000
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+# ----------------------------------------------------------------------- #
+
+alpha=70
+list=50
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=100
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=200
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=300
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=500
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=1000
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+# ----------------------------------------------------------------------- #
+
+alpha=90
+list=50
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=100
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=200
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=300
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=500
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
+
+list=1000
+output="output-limit-$limit-list-$list-alpha-$alpha-in-$instances"
+./bin/main $limit $list $alpha < ./instances/$instances > ./output/$output
+echo $output
